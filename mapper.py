e47fe5568445ddb4e87f557e7e268f3c32d0f961
Viktor Maruna <viktormaruna@gmail.com>
2014-09-27 13:10:28 +0200

    reformat

--- mapper.py
+++ mapper.py
@@ -8,27 +8,26 @@ import sys
 import rdflib
 import StringIO
 
-###############################################################################
+# ##############################################################################
 def main():
+  for line in sys.stdin:
+    try:
+      line = line.strip()
 
-	for line in sys.stdin:
-		try:
-			line = line.strip()
-			
+      if line[0] != '#':
+        for stmt in rdflib.Graph().parse(StringIO.StringIO(line), format = 'nt'):  # parse RDF format
+          stmt_type = stmt[1].split('#')[-1]  # record type (e.g. label)
+          stmt_key = stmt[0].split('/')[-1]  # record key
 
-			if line[0]!='#':				
-				for stmt in rdflib.Graph().parse(StringIO.StringIO(line), format='nt'): # parse RDF format
-					stmt_type = stmt[1].split('#')[-1] # record type (e.g. label)
-					stmt_key = stmt[0].split('/')[-1] # record key
+          # get record value
+          if stmt_type == 'label':  # labels
+            stmt_value = {stmt_type: str(stmt[2])}
 
-					# get record value
-					if stmt_type == 'label': # labels
-						stmt_value = {stmt_type:str(stmt[2])}				
+          print  '%s\t%s' % (stmt_key, stmt_value)
 
-					print  '%s\t%s' % (stmt_key, stmt_value)
+    except:
+      pass
 
-		except:
-			pass
 
 if __name__ == '__main__':
-    main()
+  main()
--- reducer.py
+++ reducer.py
@@ -11,61 +11,61 @@ from avro.datafile import DataFileReader, DataFileWriter
 from collections import defaultdict
 
 # data types in AVRO schema
-AVRO = {"label":"string", "category":"array"}
+AVRO = {"label": "string", "category": "array"}
 
 
 # preparing data types for AVRO
 def AVRO_output(inp):
+  out = defaultdict(list)
 
-	out = defaultdict(list)
+  for i in inp:
+    out[str(i.keys()[0])].append(str(i.values()[0]))
 
-	for i in inp:
-		out[str(i.keys()[0])].append(str(i.values()[0]))
+  for i in AVRO.keys():
+    if AVRO[i] == "string":
+      out[i] = ''.join(out[i])
 
-	for i in AVRO.keys():
-		if AVRO[i] == "string":
-			out[i] = ''.join(out[i])
+  return dict(out)
 
-	return dict(out)
 
-###############################################################################
+# ##############################################################################
 def main():
+  schema = avro.schema.parse(open('dbpedia.avsc').read())
+  # writer = DataFileWriter(sys.stdout, DatumWriter(), schema)
+  writer = DataFileWriter(open("out.avro", "w"), DatumWriter(), schema)
 
-	schema = avro.schema.parse(open('dbpedia.avsc').read())
-	#writer = DataFileWriter(sys.stdout, DatumWriter(), schema)
-	writer = DataFileWriter(open("out.avro", "w"), DatumWriter(), schema)
+  data = []
+  key = None
+  prev_key = None
 
-	data = []
-	key = None
-	prev_key = None
+  for line in sys.stdin:
+    try:
+      line = line.strip()
+      key, value = line.split('\t', 1)
 
-	for line in sys.stdin:  
-		try:
-			line = line.strip()
-			key, value = line.split('\t', 1)
+      # first iteraton
+      if not prev_key:
+        prev_key = key
 
-			# first iteraton			
-			if not prev_key:
-				prev_key = key
+      # if they're the same
+      if prev_key == key:
+        data.append(eval(value))
+      else:
+        writer.append(AVRO_output(data))
+        prev_key = key
+        data = []
+        data.append(eval(value))
 
-			# if they're the same
-			if prev_key == key:
-				data.append(eval(value))
-			else:
-				writer.append(AVRO_output(data))
-				prev_key = key
-				data = []
-				data.append(eval(value))
+    except:
+      pass
 
-		except:
-			pass
+  if key == prev_key:
+    # emit last key
+    writer.append(AVRO_output(data))
 
-	if key == prev_key:
-		# emit last key
-		writer.append(AVRO_output(data))
+  writer.close()
 
-	writer.close()
 
 if __name__ == '__main__':
-    main()
+  main()
 
