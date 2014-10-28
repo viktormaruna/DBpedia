9032296c1a784155c90b97ecdd04885db21138f1
Viktor Maruna <viktormaruna@gmail.com>
2014-10-28 17:41:52 +0100

    final mapper/reducer

--- mapper.py
+++ mapper.py
@@ -8,26 +8,77 @@ import sys
 import rdflib
 import StringIO
 
-# ##############################################################################
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# get record from rdf
+def record_parser(text):
+	if '#type' in text:
+		return 'type' # type
+	elif 'terms/subject' in text:
+		return 'category' # category
+	elif 'property/wikiPageUsesTemplate' in text:
+		return "template" # template
+	elif 'ontology/wikiPageDisambiguates' in text:
+		return 'disambiguates' # disambiguates
+	else:
+		return 'property' # property
+
+# get type
+def type_parser(text):
+	if 'http://dbpedia.org/ontology' in text:
+		return text.split('/')[-1] 
+	else:
+		return None	
+
+# get category
+def category_parser(text):
+	return text.split(':')[-1]
+
+# get template
+def template_parser(text):
+	return text.split(':')[-1]
+
+# get disambiguates
+def disambiguates_parser(text):
+	return text.split('/')[-1] 
+
+# get propery
+def property_parser(text):
+	return text.split('/')[-1] 
+
+###############################################################################
 def main():
-  for line in sys.stdin:
-    try:
-      line = line.strip()
 
-      if line[0] != '#':
-        for stmt in rdflib.Graph().parse(StringIO.StringIO(line), format = 'nt'):  # parse RDF format
-          stmt_type = stmt[1].split('#')[-1]  # record type (e.g. label)
-          stmt_key = stmt[0].split('/')[-1]  # record key
+	for line in sys.stdin:
+		try:
+			line = line.strip()
+		
+			if line[0]!='#': # first line				
 
-          # get record value
-          if stmt_type == 'label':  # labels
-            stmt_value = {stmt_type: str(stmt[2])}
+				for stmt in rdflib.Graph().parse(StringIO.StringIO(line), format="nt"): # parse RDF format
+					
 
-          print  '%s\t%s' % (stmt_key, stmt_value)
+					stmt_type = record_parser(stmt[1].encode('utf-8', 'ignore')) # record type
+					stmt_key = stmt[0].encode('utf-8', 'ignore').split('/')[-1] # record key
 
-    except:
-      pass
+					# value
+					if stmt_type == 'type':
+						stmt_value = {stmt_type:type_parser(stmt[2])}
+					elif stmt_type == 'category':
+						stmt_value = {stmt_type:category_parser(stmt[2])}
+					elif stmt_type == 'template':
+						stmt_value = {stmt_type:template_parser(stmt[2])}
+					elif stmt_type == 'disambiguates':
+						stmt_value = {stmt_type:disambiguates_parser(stmt[2])}
+					else:
+						stmt_value = {stmt_type:property_parser(stmt[1])}
 
+					if not None in stmt_value.values():
+						print  '%s\t%s' % (stmt_key, stmt_value) # mapper output
+
+		except Exception, err:
+			sys.stderr.write('Mapper ERROR: %s\n' % str(err))
 
 if __name__ == '__main__':
-  main()
+    main()
--- reducer.py
+++ reducer.py
@@ -5,67 +5,63 @@
 # VI: Viktor Maruna
 
 import sys
-import avro.schema
-from avro.io import DatumReader, DatumWriter
-from avro.datafile import DataFileReader, DataFileWriter
 from collections import defaultdict
+import json
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # data types in AVRO schema
-AVRO = {"label": "string", "category": "array"}
-
+AVRO = {'article':'string', 'type':'string', 'category':'array', 'template':'array', 'disambiguates':'array', 'property':'array'}
 
 # preparing data types for AVRO
 def AVRO_output(inp):
-  out = defaultdict(list)
 
-  for i in inp:
-    out[str(i.keys()[0])].append(str(i.values()[0]))
+	out = defaultdict(list) # values dictionary
 
-  for i in AVRO.keys():
-    if AVRO[i] == "string":
-      out[i] = ''.join(out[i])
+	for i in inp:
+		out[str(i.keys()[0])].append(str(i.values()[0]))
 
-  return dict(out)
+	for i in out.keys(): #  format
+		if AVRO[i] == 'string':
+			out[i] = ''.join(out[i])	
 
+	return json.dumps(dict(out), sort_keys=True) #  output JSON
 
-# ##############################################################################
+###############################################################################
 def main():
-  schema = avro.schema.parse(open('dbpedia.avsc').read())
-  # writer = DataFileWriter(sys.stdout, DatumWriter(), schema)
-  writer = DataFileWriter(open("out.avro", "w"), DatumWriter(), schema)
 
-  data = []
-  key = None
-  prev_key = None
+	data = [] # output data
+	key = None # key
+	prev_key = None # previous key
 
-  for line in sys.stdin:
-    try:
-      line = line.strip()
-      key, value = line.split('\t', 1)
+	try:
+		for line in sys.stdin:  
+			line = line.strip()
+			key, value = line.split('\t', 1) # get key, value
 
-      # first iteraton
-      if not prev_key:
-        prev_key = key
+			# first iteraton			
+			if not prev_key:
+				prev_key = key
 
-      # if they're the same
-      if prev_key == key:
-        data.append(eval(value))
-      else:
-        writer.append(AVRO_output(data))
-        prev_key = key
-        data = []
-        data.append(eval(value))
+			# if they're the same
+			if prev_key == key:
+				data.append(eval(value))
+			else:
+				data.append({'article':prev_key})
+				print AVRO_output(data) # output
+				prev_key = key # new key
+				data = []
+				data.append(eval(value)) # append
 
-    except:
-      pass
+		if key == prev_key:
+			# emit last key
+			data.append({'article':prev_key}) # append
+			print AVRO_output(data) # output
 
-  if key == prev_key:
-    # emit last key
-    writer.append(AVRO_output(data))
-
-  writer.close()
-
+	except Exception, err:
+		sys.stderr.write('Reducer ERROR (%s): %s\n' % (key, str(err)))
 
 if __name__ == '__main__':
-  main()
+    main()
 
